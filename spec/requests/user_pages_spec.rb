fbefb2a7ccc579d8a709409b13bf79c7180ceaf4
Michael Heu <mheu@clickbooq.com>
2012-07-19 15:06:12 -0400

    Finished the signup form

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -36,6 +36,14 @@ describe "UserPages" do
         expect { click_button submit }.not_to change(User, :count)
 
       end   
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_content('error') }
+      end
+
     end 
 
     describe "with valid information" do
@@ -58,6 +66,7 @@ describe "UserPages" do
         let(:user) { User.find_by_email("mheu@heudesign.com") }
 
         it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: "Welcome")}
       end
     end 
   end
